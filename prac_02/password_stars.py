b1e53cfcce2e8a1b5946a6f6343fe155fa927dda
Shae Machin <shaemachin@hotmail.com>
2025-09-26 20:21:02 +1000

    Refactor password check program to use functions

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -1,15 +1,29 @@
 """Print asterisks as long as word"""
 
-print("Enter a word longer than 6 letters")
-word = input("Word: ")
-word_length = len(word)
 
-# ensure word is longer than 6 letters
-while word_length <=5:
-   print(f"Not long enough, try again")
-   word = input("Word: ")
-else:
+def main():
+    print("Enter a password longer than 6 letters")
+    password_length = get_password()
 
-# correct length entered
-total_stars = "*" * word_length
-print(total_stars)
+    # ensure word is longer than 6 letters
+    while password_length <= 6:
+        print(f"Not long enough, try again")
+        password_length = get_password()
+
+    # correct length entered
+    else:
+        password_stars(password_length)
+
+
+def password_stars(password_length: int):
+    total_stars = "*" * password_length
+    print(total_stars)
+
+
+def get_password() -> int:
+    password = input("Password: ")
+    password_length = len(password)
+    return password_length
+
+
+main()
