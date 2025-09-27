515877092a3a26e7dbeefc1c12a1779b0452544a
Shae Machin <shaemachin@hotmail.com>
2025-09-27 12:18:11 +1000

    Add random score program

--- prac_02/score.py
+++ prac_02/score.py
@@ -2,17 +2,38 @@
 CP1404/CP5632 - Practical
 Broken program to determine score status
 """
+
+import random
+
+
 # The intention is that the score must be between 0 and 100 inclusive;
 # 90 or more is excellent;
 # 50 or more is a pass;
 
-score = float(input("Enter score: "))
 
-if score < 0 or score > 100:
-    print("Invalid score")
-elif score >= 90:
-    print("Excellent")
-elif score >= 50:
-    print("Passable")
-else:
-    print("Bad")
+def main():
+    user_input = input("Enter a score (or press enter for a random score): ")
+
+    if user_input == "":
+        # no input received from user
+        score = random.randint(0, 100)
+        print(f"no score was given, here is your random score: {score}")
+    else:
+        # input received from user and evaluated
+        score = float(user_input)
+
+    evaluate_score(score)
+
+
+def evaluate_score(score: float):
+    if score < 0 or score > 100:
+        print("Invalid score")
+    elif score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+
+main()
