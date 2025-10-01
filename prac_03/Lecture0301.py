662fdcfcaec8cb319b64acc79d633d0bc48d5c13
Shae Machin <shaemachin@hotmail.com>
2025-10-01 21:16:25 +1000

    Add filename.txt

--- prac_03/Lecture0301.py
+++ prac_03/Lecture0301.py
@@ -1,13 +1,27 @@
 """information provided during Lecture 3
+Guessing game with files and exceptions
 """
 
+FILENAME = "secret.txt"
+
+
+
+
+
 def main():
-    secret = 6
+    secret = load_number(FILENAME)
     guess = int(input("? "))
     while guess != secret:
         print("Guess again!")
         guess = int(input("? "))
     print("You got it")
+
+def load_number(filename):
+    """Load integer from file filename"""
+    infile = open(filename, "r")
+    number = int(infile.read())
+    infile.close()
+    return number
 
 main()
 
