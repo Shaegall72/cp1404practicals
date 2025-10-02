89649a66f41983243f797dcd5fde4491d99420ba
Shae Machin <shaemachin@hotmail.com>
2025-10-02 17:34:56 +1000

    Add filename.txt

--- prac_03/Lecture0301.py
+++ prac_03/Lecture0301.py
@@ -5,23 +5,54 @@ Guessing game with files and exceptions
 FILENAME = "secret.txt"
 
 
-
-
-
 def main():
     secret = load_number(FILENAME)
-    guess = int(input("? "))
+    guess = get_valid_number()
+
     while guess != secret:
         print("Guess again!")
-        guess = int(input("? "))
+        guess = get_valid_number()
     print("You got it")
+
+    """
+    is_valid_input = False
+while not is_valid_input:
+    try:
+        age = int(input("Age: "))
+        if age < 0:
+            print("Age must be >= 0")
+        else:
+            is_valid_input = True
+    except ValueError:
+        print("Invalid (not an integer)")
+print("Next year you will be", age + 1)"""
+
+
+def get_valid_number() -> int:
+    is_valid_input = False
+    while not is_valid_input:
+        try:
+            guess = int(input("Guess: "))
+            is_valid_input = True
+        except ValueError:
+            print("Invalid integer")
+    return guess  # no problem with potential undefined variable
+
 
 def load_number(filename):
     """Load integer from file filename"""
     infile = open(filename, "r")
-    number = int(infile.read())
-    infile.close()
+    try:
+        number = int(infile.read())
+    except ValueError:
+        print(f"Inbalid contents in {filename}")
+        number = 6
+    except FileNotFoundError:
+        print(f"This {filename} not found")
+        number = 4
+    else:
+        infile.close()
     return number
 
-main()
 
+main()
